0473de2cbfc974764b24a042fafb138ee7bd5420
Dairaku <dairaku051018@gmail.com>
2019-04-30 22:11:05 +0900

    [fix]timestampからdatetime型へ

--- db/migrate/20190427193201_addcolumn_order_histories.rb
+++ db/migrate/20190427193201_addcolumn_order_histories.rb
@@ -1,6 +1,6 @@
 class AddcolumnOrderHistories < ActiveRecord::Migration
   def change
-    add_column :order_histories, :is_telled_timestamp, :timestamps
-    add_column :order_histories, :is_cancelled_timestamp, :timestamps
+    add_column :order_histories, :is_telled_timestamp, :datetime
+    add_column :order_histories, :is_cancelled_timestamp, :datetime
   end
 end
